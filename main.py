b370aa5f0b9a46b2bc9656ee45582a49919a350b
Bruno Vicente <brunovicente.lima@ifma.edu.br>
2020-01-13 16:28:25 -0300

    Main
    Mudança no main

--- main.py
+++ main.py
@@ -8,19 +8,20 @@ from keras.utils import np_utils
 from DEC import DeepEmbeddingClustering
 
 dec = DeepEmbeddingClustering(10, 784)
-dec.initialize(X)
+
 
 sca = MinMaxScaler()
 tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
 
-dados = pd.read_csv('d:/basedados/mnist.csv')
+dados = pd.read_csv('C:/Users/Servidor-LSI/Documents/Bruno Vicente/bases/mnist.csv')
 X = sca.fit_transform(dados.drop(['classe'], axis=1).values)
 Y = dados['classe'].values
 
+dec.initialize(X)
 
-sdc = SemiClustering(10, np.size(X, 1))
-sdc.treinar(X, 300)
-sdc.cluster(X)
+#sdc = SemiClustering(10, np.size(X, 1))
+#sdc.treinar(X, 300)
+#sdc.cluster(X)
 #y_treino = sdc.encoder.predict(X)
 #y_treino = sdc.p_mat(y_treino)
 #y_treino = np.argmax(y_treino, axis=1)
@@ -28,9 +29,11 @@ sdc.cluster(X)
 
 #sdc.DEC.fit(X, y_treino, epochs=100)
 
-preditas = sdc.DEC.predict_classes(X)
+dec.cluster(X)
 
 Xt = tsne.fit_transform(X)
+preditas = dec.DEC.predict_classes(X)
+acuracia = dec.cluster_acc(Y, preditas)
 
 cores = ['#000000', '#0000FF', '#7FFFD4', '#008000', '#CD853F', '#8B008B', '#FF0000','#FFA500', '#FFFF00', '#FF1493']
 for i, x in enumerate(Xt):
